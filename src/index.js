b0e790bfd487b111f1616303af46279428640540
j-kyoda <j.kyoda@gmail.com>
2026-07-05 23:12:16 +0900

    Fix: add missing module export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { ResourceLoader } from '@/components/ResourceLoader.js'
+import { GltfModel } from '@/components/GltfModel.js'
 import { VrmModel } from '@/components/VrmModel.js'
 import ThreeFrame from '@/components/ThreeFrame.vue'
+import GltfControl from '@/components/GltfControl.vue'
 import VroidControl from '@/components/VroidControl.vue'
 import VroidExpression from '@/components/VroidExpression.vue'
 import VroidModel from '@/components/VroidModel.vue'
@@ -11,6 +13,8 @@ import VroidVrm from '@/components/VroidVrm.vue'
 export {
   ResourceLoader,
   ThreeFrame,
+  GltfModel,
+  GltfControl,
   VrmModel,
   VroidControl,
   VroidExpression,
