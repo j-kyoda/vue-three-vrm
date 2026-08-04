597b7e0e0b7360f2669997eec190b35ce20510a3
j-kyoda <j.kyoda@gmail.com>
2026-08-04 22:14:11 +0900

    Feat: Add helper method to GLTF model

--- src/components/GltfControl.vue
+++ src/components/GltfControl.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { onMounted, ref, watch } from 'vue'
 
+import * as THREE from 'three'
+
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 import { GltfModel } from '@/components/GltfModel.js'
 
-const emits = defineEmits(['loading', 'loaded'])
+const emits = defineEmits(['loading', 'loaded', 'error'])
 
 const props = defineProps({
   url: {
@@ -19,12 +21,47 @@ const props = defineProps({
   name: {
     type: String,
     default: null
+  },
+  scale: {
+    type: [Object, null],
+    default: null
+  },
+  position: {
+    type: [Object, null],
+    default: null
+  },
+  rotation: {
+    type: [Object, null],
+    default: null
+  },
+  wrap: {
+    type: [Object, null],
+    default: null
+  },
+  repeat: {
+    type: [Object, null],
+    default: null
   }
 })
+
+const WRAP_MODES = {
+  'REPEAT': THREE.RepeatWrapping,
+  'CLAMP_TO_EDGE': THREE.ClampToEdgeWrapping,
+  'MIRRORED_REPEAT': THREE.MirroredRepeatWrapping,
+}
 
 const gltfModel = ref(null)
 gltfModel.value = new GltfModel()
 
+const getWrapMode = (mode) => {
+  if (typeof mode == 'number') {
+    return mode
+  }
+  if (mode in WRAP_MODES) {
+    return WRAP_MODES[mode]
+  }
+  emits('error', `Unknown wrap mode ${mode}`)
+}
 
 const _loadGltf = async (url) => {
   const loader = new GLTFLoader()
@@ -43,6 +80,39 @@ const loadGltf = async (url) => {
   const data = await _loadGltf(url)
   if (data) {
     gltfModel.value.setModel(data)
+    // scale
+    if (props.scale) {
+      gltfModel.value.setScale(props.scale)
+    } else {
+      gltfModel.value.setScale({'x': 1, 'y': 1, 'z': 1})
+    }
+    // position
+    if (props.position) {
+      gltfModel.value.setPosition(props.position)
+    } else {
+      gltfModel.value.setPosition({'x': 0, 'y': 0, 'z': 0})
+    }
+    // rotation
+    if (props.rotation) {
+      gltfModel.value.setRotation(props.rotation)
+    } else {
+      gltfModel.value.setRotation({'x': 0, 'y': 0, 'z': 0})
+    }
+    // texture
+    const wrap = {'s': '', 't': ''}
+    if (props.wrap) {
+      wrap.s = getWrapMode(props.wrap.s)
+      wrap.t = getWrapMode(props.wrap.t)
+    } else {
+      wrap.s = getWrapMode('CLAMP_TO_EDGE')
+      wrap.t = getWrapMode('CLAMP_TO_EDGE')
+    }
+    const repeat = {'u': 0, 'v': 0}
+    if (props.repeat) {
+      repeat.u = props.repeat.u
+      repeat.v = props.repeat.v
+    }
+    gltfModel.value.setTexture(wrap, repeat)
     emits('loaded', props.name, gltfModel.value)
   } else {
     emits('loaded', props.name, null)
@@ -76,6 +146,24 @@ watch(() => props.data, () => {
 watch(() => props.url, () => {
   if (props.url) {
     loadGltf(props.url)
+  }
+})
+
+watch(() => props.scale, () => {
+  if (gltfModel.value) {
+    gltfModel.value.setScale(props.scale)
+  }
+})
+
+watch(() => props.position, () => {
+  if (gltfModel.value) {
+    gltfModel.value.setPosition(props.position)
+  }
+})
+
+watch(() => props.rotation, () => {
+  if (gltfModel.value) {
+    gltfModel.value.setRotation(props.rotation)
   }
 })
 </script>
--- src/components/GltfModel.js
+++ src/components/GltfModel.js
@@ -14,37 +14,81 @@ class GltfModel {
   }
 
   setPosition(pos) {
-    if (this.gltf) {
+    if (!this.gltf) {
+      return
+    }
+    if (pos) {
       this.gltf.scene.position.set(pos.x, pos.y, pos.z)
+    } else {
+      this.gltf.scene.position.set(0, 0, 0)
     }
   }
 
   getPosition() {
-    if (this.gltf) {
-      return {
-        x: this.gltf.scene.position.x,
-        y: this.gltf.scene.position.y,
-        z: this.gltf.scene.position.z,
-      }
+    if (!this.gltf) {
+      return null
     }
-    return null
+    return {
+      x: this.gltf.scene.position.x,
+      y: this.gltf.scene.position.y,
+      z: this.gltf.scene.position.z,
+    }
   }
 
   setRotation(rot) {
-    if (this.gltf) {
+    if (!this.gltf) {
+      return
+    }
+    if (rot) {
       this.gltf.scene.rotation.set(rot.x, rot.y, rot.z)
+    } else {
+      this.gltf.scene.rotation.set(0, 0, 0)
     }
   }
 
   getRotation() {
-    if (this.gltf) {
-      return {
-        x: this.gltf.scene.rotation.x,
-        y: this.gltf.scene.rotation.y,
-        z: this.gltf.scene.rotation.z,
-      }
+    if (!this.gltf) {
+      return null
     }
-    return null
+    return {
+      x: this.gltf.scene.rotation.x,
+      y: this.gltf.scene.rotation.y,
+      z: this.gltf.scene.rotation.z,
+    }
+  }
+
+  setScale(scale) {
+    if (!this.gltf) {
+      return
+    }
+    if (scale) {
+      this.gltf.scene.scale.set(scale.x, scale.y, scale.z)
+    } else {
+      this.gltf.scene.scale.set(1, 1, 1)
+    }
+  }
+
+  setTexture(wrap, repeat) {
+    if (!this.gltf) {
+      return
+    }
+    this.gltf.scene.traverse((child) => {
+      if (child.isMesh && child.material.map) {
+        // set Wrapping mode to Repeat
+        if (wrap) {
+          child.material.map.wrapS = wrap.s
+          child.material.map.wrapT = wrap.t
+        }
+
+        // set Repeat count
+        if (repeat) {
+          child.material.map.repeat.set(repeat.u, repeat.v)
+        }
+
+        // set update flag
+        child.material.map.needsUpdate = true
+      }
+    })
   }
 }
 
